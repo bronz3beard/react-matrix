63d59ec89a56f024ed894ab0995190ccf642d778
bronz3beard <info2rory@gmail.com>
2021-10-07 22:54:22 +1000

    remove old eslint stuff

--- src/functions.js
+++ src/functions.js
@@ -1,7 +1,3 @@
-/* eslint-disable no-prototype-builtins */
-/* eslint-disable no-useless-escape */
-/* eslint-disable no-unused-expressions */
-
 export const groupObjectsByProp = (array, prop) => {
   const map = new Map(Array.from(array, (obj) => [obj[prop], []]));
 
