ce32a7de4cb4f9ea8c13d8949a33ebac9596c730
Rory Fairweather-Neylan <rfairweatherneylan@console.com.au>
2023-05-16 22:05:36 -0300

    move readme to docs build no publish

--- dist/helpers/getStyles.js
+++ dist/helpers/getStyles.js
@@ -4,17 +4,14 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports.getTableStyles = exports.getTableBoarder = exports.getHeaderTitleStyles = exports.getHeaderSubTitleStyles = exports.getHeaderRowStyles = exports.getHeaderPrimaryTitleStyles = exports.getCustomStyles = exports.getContainerStyles = void 0;
-
+require("core-js/modules/es.symbol.description.js");
 function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); enumerableOnly && (symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; })), keys.push.apply(keys, symbols); } return keys; }
-
 function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = null != arguments[i] ? arguments[i] : {}; i % 2 ? ownKeys(Object(source), !0).forEach(function (key) { _defineProperty(target, key, source[key]); }) : Object.getOwnPropertyDescriptors ? Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)) : ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } return target; }
-
-function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
-
+function _defineProperty(obj, key, value) { key = _toPropertyKey(key); if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+function _toPropertyKey(arg) { var key = _toPrimitive(arg, "string"); return typeof key === "symbol" ? key : String(key); }
+function _toPrimitive(input, hint) { if (typeof input !== "object" || input === null) return input; var prim = input[Symbol.toPrimitive]; if (prim !== undefined) { var res = prim.call(input, hint || "default"); if (typeof res !== "object") return res; throw new TypeError("@@toPrimitive must return a primitive value."); } return (hint === "string" ? String : Number)(input); }
 const getCustomStyles = hasStyle => !hasStyle ? {} : hasStyle;
-
 exports.getCustomStyles = getCustomStyles;
-
 const getContainerStyles = (hasStyle, styles) => !hasStyle ? _objectSpread({}, getCustomStyles(styles)) : _objectSpread({
   top: "".concat(50, "%"),
   left: "".concat(50, "%"),
@@ -23,48 +20,35 @@ const getContainerStyles = (hasStyle, styles) => !hasStyle ? _objectSpread({}, g
   position: "absolute",
   transform: "translate(".concat(-50, "%, ").concat(-50, "%)")
 }, styles);
-
 exports.getContainerStyles = getContainerStyles;
-
 const getTableBoarder = hasStyle => !hasStyle ? {} : {
   border: "".concat(1, "px solid black")
 };
-
 exports.getTableBoarder = getTableBoarder;
-
 const getTableStyles = (hasStyle, styles) => !hasStyle ? _objectSpread({}, getCustomStyles(styles)) : _objectSpread({
   width: "".concat(70, "rem")
 }, styles);
-
 exports.getTableStyles = getTableStyles;
-
 const getHeaderPrimaryTitleStyles = (hasStyle, styles) => !hasStyle ? _objectSpread({}, getCustomStyles(styles)) : _objectSpread({
   borderWidth: "".concat(0),
   padding: "".concat(1, "rem ", 0, " ", 1, "rem ", 0)
 }, styles);
-
 exports.getHeaderPrimaryTitleStyles = getHeaderPrimaryTitleStyles;
-
 const getHeaderRowStyles = (hasStyle, styles) => !hasStyle ? _objectSpread({}, getCustomStyles(styles)) : _objectSpread({
   borderColor: "black",
   borderStyle: "solid",
   padding: "".concat(10, "rem ", 0, " ", 0, " ", 0),
   borderWidth: "".concat(0, " ", 0, " ", 0, " ", 10, "px")
 }, styles);
-
 exports.getHeaderRowStyles = getHeaderRowStyles;
-
 const getHeaderTitleStyles = (hasStyle, styles) => !hasStyle ? _objectSpread({}, getCustomStyles(styles)) : _objectSpread({
   padding: "".concat(0, " ", 0.5, "rem ", 0, " ", 0.5, "rem"),
   backgroundColor: "light-grey",
   border: "".concat(1, "px solid black")
 }, styles);
-
 exports.getHeaderTitleStyles = getHeaderTitleStyles;
-
 const getHeaderSubTitleStyles = (hasStyle, styles) => !hasStyle ? _objectSpread({}, getCustomStyles(styles)) : _objectSpread({
   fontSize: "".concat(0.9, "em"),
   fontWeight: "normal"
 }, styles);
-
 exports.getHeaderSubTitleStyles = getHeaderSubTitleStyles;
