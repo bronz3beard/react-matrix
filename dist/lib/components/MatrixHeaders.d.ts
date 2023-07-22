380249cb41ab1d8fdd20d413b800403720a7efd2
Rory <38899587+bronz3beard@users.noreply.github.com>
2023-07-23 08:41:38 +1000

    Delete dist directory

--- dist/lib/components/MatrixHeaders.d.ts
+++
@@ -1,4 +0,0 @@
-import { FC } from 'react';
-import { MatrixHeaderProps } from '../types';
-declare const MatrixHeaders: FC<MatrixHeaderProps>;
-export default MatrixHeaders;
--- dist/lib/utils/functions.d.ts
+++
@@ -1,4 +0,0 @@
-export declare const groupObjectsByProp: <T>(array: T[], prop: keyof T) => T[][];
-export declare const capitaliseFirstCharacter: (stringValue: string) => string;
-export declare const capitaliseString: (stringValue: string) => string;
-export declare const callAll: <T extends (...args: never[]) => void>(...fns: T[]) => (...args: Parameters<T>) => void;
